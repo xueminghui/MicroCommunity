c7d958be5a8e09fed71f290292c8662571e72942
java110 <928255095@qq.com>
2022-11-04 10:42:09 +0000

    优化代码
    Signed-off-by: java110 <928255095@qq.com>

--- service-user/src/main/java/com/java110/user/dao/IRoleCommunityV1ServiceDao.java
+++ service-user/src/main/java/com/java110/user/dao/IRoleCommunityV1ServiceDao.java
@@ -74,4 +74,8 @@ public interface IRoleCommunityV1ServiceDao {
      */
     int queryRoleCommunitysCount(Map info);
 
+    int queryRoleCommunitysNameCount(Map info);
+
+    List<Map>  queryRoleCommunitysName(Map info);
+
 }
--- service-user/src/main/java/com/java110/user/dao/impl/RoleCommunityV1ServiceDaoImpl.java
+++ service-user/src/main/java/com/java110/user/dao/impl/RoleCommunityV1ServiceDaoImpl.java
@@ -109,4 +109,23 @@ public class RoleCommunityV1ServiceDaoImpl extends BaseServiceDao implements IRo
     }
 
 
+ @Override
+    public int queryRoleCommunitysNameCount(Map info) {
+        logger.debug("查询 queryRoleCommunitysNameCount 入参 info : {}", info);
+
+        List<Map> businessRoleCommunityInfos = sqlSessionTemplate.selectList("roleCommunityV1ServiceDaoImpl.queryRoleCommunitysNameCount", info);
+        if (businessRoleCommunityInfos.size() < 1) {
+            return 0;
+        }
+
+        return Integer.parseInt(businessRoleCommunityInfos.get(0).get("count").toString());    }
+
+    @Override
+    public List<Map> queryRoleCommunitysName(Map info) {
+        logger.debug("查询 getRoleCommunityInfo 入参 info : {}", info);
+
+        List<Map> businessRoleCommunityInfos = sqlSessionTemplate.selectList("roleCommunityV1ServiceDaoImpl.queryRoleCommunitysName", info);
+
+        return businessRoleCommunityInfos;
+    }
 }
