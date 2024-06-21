2c2b4559de0cd761fc327d7be75786cefb7273eb
java110 <928255095@qq.com>
2024-06-21 03:42:41 +0000

    优化退费审核
    Signed-off-by: java110 <928255095@qq.com>

--- service-fee/src/main/java/com/java110/fee/cmd/returnPayFee/UpdateReturnPayFeeCmd.java
+++ service-fee/src/main/java/com/java110/fee/cmd/returnPayFee/UpdateReturnPayFeeCmd.java
@@ -173,9 +173,9 @@ public class UpdateReturnPayFeeCmd extends Cmd {
         Assert.hasKeyAndValue(reqJson, "state", "state不能为空");
         Assert.hasKeyAndValue(reqJson, "feeId", "feeId不能为空");
         if (reqJson.containsKey("cycles")) {
-            String cycles = reqJson.getString("cycles");
-            if (!cycles.startsWith("-")) {
-                throw new IllegalArgumentException("退费周期必须负数");// 这里必须传入负数，否则费用自动相加不会退费
+             double cycles = reqJson.getDouble("cycles");
+            if (cycles > 0) {
+                throw new CmdException("退费周期必须负数");// 这里必须传入负数，否则费用自动相加不会退费
             }
         }
         FeeDetailDto feeDetailDto = new FeeDetailDto();
